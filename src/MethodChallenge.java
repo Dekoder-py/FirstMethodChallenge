53b468d5cc3a58bba00002f1853e295cdf888169
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-14 05:52:35 +1300

    finish the challenge and add tests

--- src/MethodChallenge.java
+++ src/MethodChallenge.java
@@ -1,11 +1,20 @@
 public class MethodChallenge {
 
     public static void main(String[] args) {
-//        displayHighScorePosition();
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", highScorePosition);
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Bob", highScorePosition);
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Percy", highScorePosition);
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Gilbert", highScorePosition);
+        highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("James", highScorePosition);
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition){
-        System.out.println(playerName + "managed to get into position " + highScorePosition + " on the high score list");
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score list");
     }
 
     public static int calculateHighScorePosition(int playerScore){
